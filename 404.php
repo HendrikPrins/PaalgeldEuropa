09742e008db484eadf5a68a2aad9ac036ba145e6
Steven <stevenkema@gmail.com>
2014-12-10 17:54:23 +0100

    404 redirect-pagina gemaakt

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+require_once('inc/config.php');
+$_loadChosen = false;
+beginPage('',false);
+?>
+<div class="page404">
+	<div class="container">
+			<div class="center-block">
+			<h1>404</h1>
+			<p>Page not Found</p>
+				<a class="btn btn-default btn-lg" href="index.php" role="button">Go home</a>
+		</div>
+	</div>
+</div>
+
+
