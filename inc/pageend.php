b5b994745c23f3ce416f813845fa6266b8675947
Steven <stevenkema@gmail.com>
2014-12-10 16:06:32 +0100

    Stuk HTML dat verkeerd in pageend.php stond in index.php gestopt.

--- inc/pageend.php
+++ inc/pageend.php
@@ -2,48 +2,12 @@
 if($_inContainer){
   ?>
                      </div>
-
                 </div>
             </div>
         </div>
   <?php
   }else{
   ?>
-
-
-<div class="info">
-                <div class="container">
-                    <h2>Paalgeld Europa</h2>
-                        <div class="row">
-                            <div class="col-md-4">
-                              <h3>About the Website</h3>
-                              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                            </div>
-                            <div class="col-md-4">
-                              <h3>About the assignment</h3>
-                              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                           </div>
-                            <div class="col-md-4">
-                              <h3>Something else here</h3>
-                              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-
-            <div class="info2">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h2>Some more content here</h2>
-                            <p>Ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="footer">
                 <div class="container">
                     <div class="row">
