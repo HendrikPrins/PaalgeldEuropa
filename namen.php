8c248d18374aeefcb86ebe4729aefe1dd4315708
Reinard van Dalen <info@vandalenwebdesign.nl>
2014-12-12 12:37:16 +0100

    Formulieren aangepast

--- namen.php
+++ namen.php
@@ -5,32 +5,44 @@ beginPage('Paalgeld Europa - Namen', true, 'Onderzoek a.h.v. namen');
 ?>
 <form class="form-horizontal" role="form" action="namenresult.php" method="post">
                           <div class="form-group">
-                            <label for="inputAchternaam" class="col-sm-2 control-label">Achternaam</label>
+                            <label for="inputname" class="col-sm-2 control-label">Name</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputAchternaam" placeholder="Achternaam">
-                          </div>
-                            </div>
-                          <div class="form-group">
-                            <label for="inputVoornaam" class="col-sm-2 control-label">Voornaam</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputVoornaam" placeholder="Voornaam">
-                          </div>
-                            </div>
-                            <div class="form-group">
-                            <label for="inputDatum" class="col-sm-2 control-label">Datum</label>
-                            <div class="col-xs-2">
-                            <input type="number" min="0" max="9999" class="form-control" id="inputBegindatum" placeholder="Begin">
-                            </div>
-                            <div class="col-xs-2">
-                            <input type="number" min="0" max="9999" class="form-control" id="inputEinddatum" placeholder="Eind">
+                            <input type="text" class="form-control" id="inputName" placeholder="Name">
                             </div>
                             </div>
                             <div class="form-group">
-                            <label for="inputPlaatsvertrek" class="col-sm-2 control-label">Plaats van vertrek</label>
+                            <label for="checkSearch" class="col-sm-2 control-label">Search Exact</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPlaatsvertrek" placeholder="Plaats">
-                          </div>
+                            <div class="checkbox">
+                                <label>
+                                  <input type="checkbox"> I want to search exact
+                                </label>
+                              </div>
                             </div>
+                            </div>
+                            <div class="form-group">
+                            <label for="inputStartDate" class="col-sm-2 control-label">Date</label>
+                            <div class="col-xs-2">
+                            <input type="date" min="1742-01-01" max="1787-12-31" class="form-control" id="inputStartDate" placeholder="Start">
+                            </div>
+                            <div class="col-xs-2">
+                            <input type="date" min="1742-01-01" max="1787-12-31" class="form-control" id="inputEndDate" placeholder="End">
+                            </div>
+                            </div>
+                            <div class="form-group">
+                              <label for="inputPlaats" class="col-sm-2 control-label">Place of departure</label>
+                                <div class="col-sm-10">
+                                  <select name="inputPlaats" data-placeholder="Choose a place" class="chosen-select" style="width:350px;" tabindex="2">
+                                    <?php
+                                    $query = "SELECT departurePort FROM paalgeldwest GROUP BY departurePort ORDER BY departurePort";
+                                    $res = mysql_query($query);
+                                      while($row = mysql_fetch_array($res)){
+                                        echo '<option value='.$row['departurePort'].'>'.$row['departurePort'].'</option>';
+                                        }
+                                    ?>  
+                                  </select>
+                                </div>
+                              </div>
                           <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
                         <button type="submit" class="btn btn-default">Onderzoek</button>
