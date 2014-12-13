d2edb19024b50a86818ddc11626975b93005ab97
Hendrik Prins <hawprins@gmail.com>
2014-12-13 16:26:13 +0100

    Kaart toegevoegd aan captain detail overzicht

--- table_captains.php
+++ table_captains.php
@@ -1,41 +1,40 @@
 <?php
 require('inc/config.php');
+$_loadGoogleMaps = true;
 beginPage();
 
 if(isset($_GET['id'])){
-  //
   $captain = str_replace('_', ' ', $_db->real_escape_string($_GET['id']));
-  $query = "SELECT * FROM paalgeldEur, ports WHERE paalgeldEur.portCode = ports.portCode AND fullNameCaptain = '".$captain."'";   
-  $res = $_db->query($query);
-  if($res == null || $res->num_rows == 0){
+  $resDetail = $_db->query("SELECT * FROM paalgeldEur WHERE fullNameCaptain = '".$captain."' LIMIT 1");
+  if($resDetail == null || $resDetail->num_rows == 0){
     echo '<div class="alert alert-warning" role="alert"><strong>Error.</strong> No captain names with name <strong>'.$_GET['id'].'</strong> found. <a class="alert-link" href="#" onclick="history.go(-1)">Go Back</a><br>Error code: '.$_db->error.'</div>';
   }else{
     // details
-    download_knop($query);
 	echo '<table class="table">';
-    $row = $res->fetch_assoc();
-	$year = substr($row['date'], 0, -6);	
-	$month = substr($row['date'], 5, -3);
-	$day = substr($row['date'], 8);
-    $captain = str_replace(' ', '_', $row['fullNameCaptain']);
-    echo '<tr><td>Full name</td><td><a href="table_captains.php?id='.$captain.'">'.$row['fullNameCaptain'].'</a></td></tr>';
-    echo '<tr><td>First name</td><td>'.$row['firstNameCaptain'].'</td></tr>';
-    echo '<tr><td>Last Name</td><td>'.$row['lastNameCaptain'].'</td></tr>';
+    $rowDetail = $resDetail->fetch_assoc();
+    $captain = str_replace(' ', '_', $rowDetail['fullNameCaptain']);
+    echo '<tr><td>Full name</td><td><a href="table_captains.php?id='.$captain.'">'.$rowDetail['fullNameCaptain'].'</a></td></tr>';
+    echo '<tr><td>First name</td><td>'.$rowDetail['firstNameCaptain'].'</td></tr>';
+    echo '<tr><td>Last Name</td><td>'.$rowDetail['lastNameCaptain'].'</td></tr>';
     echo '</table>';
-// de arrivals
+
+    $query = "SELECT idEur, date, fullNameCaptain, firstNameCaptain, lastNameCaptain, portName, ports.portCode AS pCode, lat, lng FROM paalgeldEur, ports WHERE paalgeldEur.portCode = ports.portCode AND fullNameCaptain = '".$rowDetail['fullNameCaptain']."'";
+    include_once('inc/module_map.php');
+    // In de kaart willen we alleen unieke ports, dus nog een group by pCode en tellen
+    makeGoogleMapsQuery("SELECT COUNT(*) AS portCount, sub.* FROM (".$query.") AS sub GROUP BY pCode", 'portCount', 'pCode');
+    // de arrivals
+    download_knop($query);
     echo '<b>Arrivals</b>';
     echo '<table class="table table-hover">';
-    echo '<tr><th>Arrival id</th><th>Date</th><th>First name</th><th>Last Name</th><th>Departure port</th></tr>';
-    echo '<tr><td><a href="table_arrivals.php?id='.$row['idEur'].'">'.$row['idEur'].'</a></td><td><a href="table_date.php?year='.$year.'">'.$year.'</a>-<a href="table_date.php?month='.$month.'">'.$month.'</a>-<a href="table_date.php?day='.$day.'">'.$day.'</a></td><td>'.$row['firstNameCaptain'].'</td><td>'.$row['lastNameCaptain'].'</td><td><a href="table_ports.php?portCode='.$row['portCode'].'">'.$row['portName'].'</a></td></tr>';
-    if($res->num_rows > 1){
-      while($row = $res->fetch_assoc()){
-	    $year = substr($row['date'], 0, -6);	
-	    $month = substr($row['date'], 5, -3);
-	    $day = substr($row['date'], 8);
-        echo '<tr><td><a href="table_arrivals.php?id='.$row['idEur'].'">'.$row['idEur'].'</a></td><td><a href="table_date.php?year='.$year.'">'.$year.'</a>-<a href="table_date.php?month='.$month.'">'.$month.'</a>-<a href="table_date.php?day='.$day.'">'.$day.'</a></td><td>'.$row['firstNameCaptain'].'</td><td>'.$row['lastNameCaptain'].'</td><td><a href="table_ports.php?portCode='.$row['portCode'].'">'.$row['portName'].'</a></td></tr>';
-      }
+    echo '<tr><th>Arrival id</th><th>Date</th><th>Full name</th><th>Departure port</th></tr>';
+    $res = $_db->query($query);
+    while($row = $res->fetch_assoc()){
+        $year = substr($row['date'], 0, -6);
+        $month = substr($row['date'], 5, -3);
+        $day = substr($row['date'], 8);
+        echo '<tr><td><a href="table_arrivals.php?id='.$row['idEur'].'">'.$row['idEur'].'</a></td><td><a href="table_date.php?year='.$year.'">'.$year.'</a>-<a href="table_date.php?month='.$month.'">'.$month.'</a>-<a href="table_date.php?day='.$day.'">'.$day.'</a></td><td>'.$row['fullNameCaptain'].'</td><td><a href="table_ports.php?portCode='.$row['pCode'].'">'.$row['portName'].'</a></td></tr>';
     }
-  echo '</table>';
+    echo '</table>';
   }
  
 }else{
