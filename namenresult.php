56b1b1e203573d7999599e7d3ca2e27929d1fa75
Reinard van Dalen <info@vandalenwebdesign.nl>
2014-12-15 14:39:41 +0100

    namenresult erro

--- namenresult.php
+++ namenresult.php
@@ -15,8 +15,7 @@ require_once('inc/config.php');
 
     $_loadGoogleMaps = true;
     beginPage('Paalgeld Europa - Names', true, 'Research based on names');
-    echo '<table class="table table-hover">';
-    echo '<tr><th>Captain</th><th>Arrivals</th></tr>';
+    
     $query = "SELECT fullNameCaptain, COUNT(*) AS arrivalCount, lat, lng, ports.portCode AS pCode, portName FROM paalgeldEur, ports WHERE paalgeldEur.portCode = ports.portCode";
     if($inputName != ""){
       $query .= " AND fullNameCaptain like '$inputName'";
@@ -33,11 +32,20 @@ require_once('inc/config.php');
     }
     $query .= " GROUP BY fullNameCaptain";
     include_once('inc/module_map.php');
-    makeGoogleMapsQuery("SELECT SUM(arrivalCount) AS portSum, sub.* FROM (".$query.") AS sub GROUP BY pCode", 'portSum', 'pCode');
+
     $res = $_db->query($query);
-    while($row = $res->fetch_array()){
-        $captain = str_replace(' ', '_', $row['fullNameCaptain']);
-        echo '<tr><td><a href="table_captains.php?id='.$captain.'">'.$row['fullNameCaptain'].'</a></td><td>'.$row['arrivalCount'].'</td></tr>';
+
+    if ($res == null or $res->num_rows == 0){
+        echo "<div class='alert alert-danger' role='alert'>No results found. Try again.</div>";
+    }
+    else {
+        makeGoogleMapsQuery("SELECT SUM(arrivalCount) AS portSum, sub.* FROM (".$query.") AS sub GROUP BY pCode", 'portSum', 'pCode');
+        echo '<table class="table table-hover">';
+        echo '<tr><th>Captain</th><th>Arrivals</th></tr>';
+        while($row = $res->fetch_array()){
+            $captain = str_replace(' ', '_', $row['fullNameCaptain']);
+            echo '<tr><td><a href="table_captains.php?id='.$captain.'">'.$row['fullNameCaptain'].'</a></td><td>'.$row['arrivalCount'].'</td></tr>';
+        }
     }
 endPage();
 ?>
