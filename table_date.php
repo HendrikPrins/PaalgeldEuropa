0335a9f541fbc2d819b097450f0034b037067938
KoenEsselink <koenesselink@hotmail.com>
2014-12-19 12:58:02 +0100

    Revert "table_date.php veranderd naar drie los staande tab"
    This reverts commit 02f09c3ba2c076d419939925b0a2facd7ffb4ce1.

--- table_date.php
+++ table_date.php
@@ -70,11 +70,9 @@ if(isset($_GET['year'])){
 	}
     echo '</table>';
   } 
-}else{ 
-  echo '<div class="row">';
-  echo '<div class="col-md-3">';
+}else{
   echo '<table class="table table-hover">';
-  echo '<tr><th>Year</th></tr>';
+  echo '<tr><th>Year</th><th>Month</th><th>Day</th></tr>';
   $query1 = "SELECT * FROM paalgeldEur GROUP BY year(date)";
   $query2 = "SELECT * FROM paalgeldEur GROUP BY month(date)";
   $query3 = "SELECT * FROM paalgeldEur GROUP BY day(date)";
@@ -99,73 +97,11 @@ if(isset($_GET['year'])){
   for($i = 0;$i < max(array(count($years), count($months), count($days)));$i++){
     echo '<tr>';
     echo '<td>'.($i < count($years) ? '<a href="table_date.php?year='.$years[$i].'">'.$years[$i].'</a>' : '').'</td>';
-    echo '</tr>';
-  }
-  echo '</table>';
-  echo '</div>';
-  echo '<div class="col-md-3">';
-  echo '<table class="table table-hover">';
-  echo '<tr><th>Month</th></tr>';
-  $query1 = "SELECT * FROM paalgeldEur GROUP BY year(date)";
-  $query2 = "SELECT * FROM paalgeldEur GROUP BY month(date)";
-  $query3 = "SELECT * FROM paalgeldEur GROUP BY day(date)";
-  $res1 = $_db->query($query1);
-  $res2 = $_db->query($query2);
-  $res3 = $_db->query($query3);
-  $days = array();
-  $months = array();
-  $years = array();
-  while($row1 = $res1->fetch_assoc()){
-    $years[] = substr($row1['date'], 0, -6);
-  }
-  while($row2 = $res2->fetch_assoc()){
-    $month = substr($row2['date'], 5, -3);
-    $dateObj   = DateTime::createFromFormat('!m', $month);
-    $monthName = $dateObj->format('F');
-    $months[] = array("number" => $month, "name" => $monthName);
-  }
-  while($row3 = $res3->fetch_assoc()){
-	  $days[] = substr($row3['date'], 8);
-  }
-  for($i = 0;$i < max(array(count($years), count($months), count($days)));$i++){
-    echo '<tr>';
     echo '<td>'.($i < count($months) ? '<a href="table_date.php?month='.$months[$i]["number"].'">'.$months[$i]["name"].'</a>' : '').'</td>';
-    echo '</tr>';
-  }
-  echo '</table>';
-  echo '</div>';
-  echo '<div class="col-md-3">';
-  echo '<table class="table table-hover">';
-  echo '<tr><th>Day</th></tr>';
-  $query1 = "SELECT * FROM paalgeldEur GROUP BY year(date)";
-  $query2 = "SELECT * FROM paalgeldEur GROUP BY month(date)";
-  $query3 = "SELECT * FROM paalgeldEur GROUP BY day(date)";
-  $res1 = $_db->query($query1);
-  $res2 = $_db->query($query2);
-  $res3 = $_db->query($query3);
-  $days = array();
-  $months = array();
-  $years = array();
-  while($row1 = $res1->fetch_assoc()){
-    $years[] = substr($row1['date'], 0, -6);
-  }
-  while($row2 = $res2->fetch_assoc()){
-    $month = substr($row2['date'], 5, -3);
-    $dateObj   = DateTime::createFromFormat('!m', $month);
-    $monthName = $dateObj->format('F');
-    $months[] = array("number" => $month, "name" => $monthName);
-  }
-  while($row3 = $res3->fetch_assoc()){
-	  $days[] = substr($row3['date'], 8);
-  }
-  for($i = 0;$i < max(array(count($years), count($months), count($days)));$i++){
-    echo '<tr>';
     echo '<td>'.($i < count($days) ? '<a href="table_date.php?day='.$days[$i].'">'.$days[$i].'</a>' : '').'</td>';
     echo '</tr>';
   }
   echo '</table>';
-  echo '</div>';
-  echo '</div>';
 }
 endPage();
 ?>
