04dfb71dfd986993eb2cd61b36a7e15b9cb99fae
Hendrik Prins <hawprins@gmail.com>
2014-12-19 14:48:38 +0100

    Revert "Revert "table_date.php veranderd naar drie los staande tab""
    This reverts commit 0335a9f541fbc2d819b097450f0034b037067938.
    
    Conflicts:
    	table_date.php

--- table_date.php
+++ table_date.php
@@ -10,7 +10,7 @@ if(isset($_GET['year'])){
     echo '<div class="alert alert-warning" role="alert">No arrivals with year <strong>'.$_GET['year'].'</strong> found. <a class="alert-link" href="index.php">Go back to home</a></div>'; 
   }else{
 	echo 'Arrivals in <a href="table_date.php?year='.$_GET['year'].'">'.$_GET['year'].'</a>';
-  
+
     download_knop($query);
     
 	echo '<table class="table table-hover">';
@@ -70,57 +70,104 @@ if(isset($_GET['year'])){
 	}
     echo '</table>';
   } 
-}else{
+}else{ 
+  echo '<div class="row">';
+  echo '<div class="col-md-3">';
   echo '<table class="table table-hover">';
-  echo '<tr><th>Year</th><th>Month</th><th>Day</th></tr>';
-  $resDay = $_db->query("SELECT * FROM paalgeldEur GROUP BY day(date)");
-  $resMonth = $_db->query("SELECT * FROM paalgeldEur GROUP BY month(date)");
-  $resYear = $_db->query("SELECT * FROM paalgeldEur GROUP BY year(date)");
-  echo '<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingYear">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapseYear" aria-expanded="true" aria-controls="collapseYear">
-          Years <span style="font-size:12pt;" class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-        </a>
-      </h4>
-    </div>
-    <div id="collapseYear" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingYear">
-      <div class="panel-body">
-      jaren
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingMonth">
-      <h4 class="panel-title">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseMonth" aria-expanded="false" aria-controls="collapseMonth">
-          Months <span style="font-size:12pt;" class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-        </a>
-      </h4>
-    </div>
-    <div id="collapseMonth" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingMonth">
-      <div class="panel-body">
-        maanden...
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingDays">
-      <h4 class="panel-title">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseDays" aria-expanded="false" aria-controls="collapseDays">
-          Days <span style="font-size:12pt;" class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-        </a>
-      </h4>
-    </div>
-    <div id="collapseDays" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingDays">
-      <div class="panel-body">
-      dagen
-      </div>
-    </div>
-  </div>
-</div>';
 
+
+  echo '<tr><th>Year</th></tr>';
+  $query1 = "SELECT * FROM paalgeldEur GROUP BY year(date)";
+  $query2 = "SELECT * FROM paalgeldEur GROUP BY month(date)";
+  $query3 = "SELECT * FROM paalgeldEur GROUP BY day(date)";
+  $res1 = $_db->query($query1);
+  $res2 = $_db->query($query2);
+  $res3 = $_db->query($query3);
+  $days = array();
+  $months = array();
+  $years = array();
+  while($row1 = $res1->fetch_assoc()){
+    $years[] = substr($row1['date'], 0, -6);
+  }
+  while($row2 = $res2->fetch_assoc()){
+    $month = substr($row2['date'], 5, -3);
+    $dateObj   = DateTime::createFromFormat('!m', $month);
+    $monthName = $dateObj->format('F');
+    $months[] = array("number" => $month, "name" => $monthName);
+  }
+  while($row3 = $res3->fetch_assoc()){
+	  $days[] = substr($row3['date'], 8);
+  }
+  for($i = 0;$i < max(array(count($years), count($months), count($days)));$i++){
+    echo '<tr>';
+    echo '<td>'.($i < count($years) ? '<a href="table_date.php?year='.$years[$i].'">'.$years[$i].'</a>' : '').'</td>';
+    echo '</tr>';
+  }
+  echo '</table>';
+  echo '</div>';
+  echo '<div class="col-md-3">';
+  echo '<table class="table table-hover">';
+  echo '<tr><th>Month</th></tr>';
+  $query1 = "SELECT * FROM paalgeldEur GROUP BY year(date)";
+  $query2 = "SELECT * FROM paalgeldEur GROUP BY month(date)";
+  $query3 = "SELECT * FROM paalgeldEur GROUP BY day(date)";
+  $res1 = $_db->query($query1);
+  $res2 = $_db->query($query2);
+  $res3 = $_db->query($query3);
+  $days = array();
+  $months = array();
+  $years = array();
+  while($row1 = $res1->fetch_assoc()){
+    $years[] = substr($row1['date'], 0, -6);
+  }
+  while($row2 = $res2->fetch_assoc()){
+    $month = substr($row2['date'], 5, -3);
+    $dateObj   = DateTime::createFromFormat('!m', $month);
+    $monthName = $dateObj->format('F');
+    $months[] = array("number" => $month, "name" => $monthName);
+  }
+  while($row3 = $res3->fetch_assoc()){
+	  $days[] = substr($row3['date'], 8);
+  }
+  for($i = 0;$i < max(array(count($years), count($months), count($days)));$i++){
+    echo '<tr>';
+    echo '<td>'.($i < count($months) ? '<a href="table_date.php?month='.$months[$i]["number"].'">'.$months[$i]["name"].'</a>' : '').'</td>';
+    echo '</tr>';
+  }
+  echo '</table>';
+  echo '</div>';
+  echo '<div class="col-md-3">';
+  echo '<table class="table table-hover">';
+  echo '<tr><th>Day</th></tr>';
+  $query1 = "SELECT * FROM paalgeldEur GROUP BY year(date)";
+  $query2 = "SELECT * FROM paalgeldEur GROUP BY month(date)";
+  $query3 = "SELECT * FROM paalgeldEur GROUP BY day(date)";
+  $res1 = $_db->query($query1);
+  $res2 = $_db->query($query2);
+  $res3 = $_db->query($query3);
+  $days = array();
+  $months = array();
+  $years = array();
+  while($row1 = $res1->fetch_assoc()){
+    $years[] = substr($row1['date'], 0, -6);
+  }
+  while($row2 = $res2->fetch_assoc()){
+    $month = substr($row2['date'], 5, -3);
+    $dateObj   = DateTime::createFromFormat('!m', $month);
+    $monthName = $dateObj->format('F');
+    $months[] = array("number" => $month, "name" => $monthName);
+  }
+  while($row3 = $res3->fetch_assoc()){
+	  $days[] = substr($row3['date'], 8);
+  }
+  for($i = 0;$i < max(array(count($years), count($months), count($days)));$i++){
+    echo '<tr>';
+    echo '<td>'.($i < count($days) ? '<a href="table_date.php?day='.$days[$i].'">'.$days[$i].'</a>' : '').'</td>';
+    echo '</tr>';
+  }
+  echo '</table>';
+  echo '</div>';
+  echo '</div>';
 }
 endPage();
 ?>
