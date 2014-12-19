46fc2084748347362b6d17f7ce8b4345bf681b4b
Hendrik Prins <hawprins@gmail.com>
2014-12-19 11:14:56 +0100

    Geen special chars en wel db real escape string

--- inc/module_validate.php
+++ inc/module_validate.php
@@ -2,10 +2,9 @@
 	// Input validatie functie, maakt eventueel in een html formulier geinjecteerde code onschadelijk door
 	// tags te veranderen in html entiteiten, quotes worden geescaped
 	function validate($data){
+	  global $_db;
 		$data = trim($data);
-		$data = stripslashes($data);
-		$data = addslashes($data);
-		$data = htmlspecialchars($data);
+		$data = $_db->real_escape_string($data);
 		return $data;
-		}
+	}
 ?>
