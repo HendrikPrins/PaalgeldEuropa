1ee36ec572f2124731f26fd30626d000b29beaba
KoenEsselink <koenesselink@hotmail.com>
2015-01-03 20:36:30 +0100

    haakje

--- placesresult.php
+++ placesresult.php
@@ -10,7 +10,7 @@ $areaTwo = validate($_GET['areaTwo']);
 $portOne = validate($_GET['portOne']);
 $portTwo = validate($_GET['portTwo']);
 
-$cargo = validate(urldecode($_GET['cargo']);
+$cargo = validate(urldecode($_GET['cargo']));
 $inputStartDate = validate($_GET['inputStartDate']);
 $inputEndDate = validate($_GET['inputEndDate']);
 		
