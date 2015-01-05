0109c3589274dde49cd92dcb7f1e746fbd63a4c5
Reinard van Dalen <info@vandalenwebdesign.nl>
2015-01-05 10:18:42 +0100

    To Do rij 6 Karel Beckeringh
    E-mailadres toegevoegd

--- index.php
+++ index.php
@@ -195,7 +195,7 @@ beginPage('', false);
                     <div class="row">
                         <div class="col-md-12">
                             <h2>Contact us</h2>
-                            <p>Got questions, remarks or feedback about the website or about the project itself?  Send us an email at <a href="mailto:#">first.last@example.com</a>.</p>
+                            <p>Got questions, remarks or feedback about the website or about the project itself?  Send us an email at <a href="mailto:paalgeldeuropa@reinardvandalen.nl">paalgeldeuropa@reinardvandale.nl</a>.</p>
                         </div>
                     </div>
                 </div>
