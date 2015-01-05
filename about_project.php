b4eb864fc466c1e29b6bf69b7fce8f2c4c4f328b
Reinard van Dalen <info@vandalenwebdesign.nl>
2015-01-05 10:26:47 +0100

    To Do List rij 7 en 8
    Background info oa

--- about_project.php
+++ about_project.php
@@ -7,7 +7,7 @@ beginPage('', false);
 	<div class ="container">
 		<h2>About the Project</h2>
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-7">
 				<p>
                   The Paalgeld-registers exist of portbooks covering the periods 1742, a single year, 
 					1771 to 1810, 1814 to 1828, and 1830 to 1836. These portbooks consist of two parts: 
@@ -15,11 +15,13 @@ beginPage('', false);
 					with the area that was under the monopoly of the Dutch West India Company 
 					(i.e. the Americas, and the west coast of Africa). 
 				</p>
-				
-				<p>
+                <p>
 					The books inform us of the name of the ships, the captain's name, the cargo of the ships and the tax paid per cargo, and 
 					the harbour the ship sailed from. One can search on ship name, captain name, cargo, departing harbour and period. 
                </p>   
+            </div>
+            <div class="col-md-5">
+				<img class="img-responsive" src="img/boek.jpg" />
 			</div>
 		</div>
 	</div>
